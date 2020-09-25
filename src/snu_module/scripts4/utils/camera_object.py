7652e27b3034c99a396ee53989b33b0a701ea70d
KYLE <kyuewang5056@gmail.com>
2020-09-25 16:21:44 +0900

    ..

---
+++ src/snu_module/scripts4/utils/camera_object.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""
+SNU Integrated Module
+
+    - Object Class Python Script for Fixed Camera
+
+"""
+import cv2
+import numpy as np
+from math import cos, sin, pi, atan2
+
+
+class FIXED_CAMERA(object):
+    def __init__(self, camera_id, ):
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
